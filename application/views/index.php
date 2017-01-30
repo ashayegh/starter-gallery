0660aaecdbfa3934e79836f7b962263f869e46b0
ashayegh <shayegh.software@gmail.com>
2017-01-29 17:51:20 -0800

    small fix

--- application/views/index.php
+++ application/views/index.php
@@ -8,7 +8,7 @@
             <span>
                 <ul>
                     <li><a href="/">Home</a></li>
-                    <li><a href="/gallery">Gallery</a>/li>
+                    <li><a href="/gallery">Gallery</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/welcome">welcome</a></li>
                 </ul>
